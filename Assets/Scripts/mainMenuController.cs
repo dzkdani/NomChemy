a4cf7cc2052c9f3e7b587955686441d43b62a1cb
dzkdani <33739691+dzkdani@users.noreply.github.com>
2019-10-30 21:44:33 +0700

    Progres 6
    - Win Condition
    - Lose Condition
    - Special soal belakangan
    - fixed elenmeyer spawn location

---
+++ Assets/Scripts/mainMenuController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class mainMenuController : MonoBehaviour
+{
+        [SerializeField] public List<GameObject> panel = new List<GameObject>();
+        public static int unlockedLvl = 1;
+        
+    private void Awake() {
+        turnOffOtherPanel(4); //only turn on main menu
+    }
+
+    void turnOffOtherPanel(int indexOn) {
+        foreach (var item in panel)
+         {
+             item.SetActive(false);
+         }
+         panel[indexOn].SetActive(true);
+    }
+    public void onButtonPressed(int index) {
+         turnOffOtherPanel(index);
+    }
+
+    public void onBackButton() {
+        turnOffOtherPanel(4);
+    }
+
+    public void onPlayLevel (int lvl) {
+        if (lvl <= unlockedLvl)
+        {
+            SceneManager.LoadScene("Level " + lvl);
+        } else {
+            Debug.Log("Level Terkunci");
+        }
+    }
+
+}
